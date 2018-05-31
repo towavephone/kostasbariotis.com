544f90d5b5e62d1735c25c7c6c60916aeec2ed64
towavephone <634407147@qq.com>
2018-05-31 17:30:04 +0800

    更改简历以及修复评论字体颜色

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -23,7 +23,7 @@ import { isMobile } from '../utils/common';
 export default class Template extends Component {
     constructor(props) {
         super(props);
-        this.isProduction = process.env.NODE_ENV === 'production';
+        this.isProduction = true;
         this.state = {
             collapse: false,
             collapseFirst: true,
